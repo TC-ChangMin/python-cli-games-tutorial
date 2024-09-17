a1fa91aacd2867ae2ecb854514fd14d578ce77a9
Nick Nguyen <nickmn2006@gmail.com>
2024-09-16 20:48:51 -0700

    added bnc stretch

--- bnc.py
+++ bnc.py
@@ -12,8 +12,24 @@ computer = roles[randint(0,2)]
 player = False
 
 while player == False:
+    print("Get ready to play Bear, Ninja, Cowboy!")
+    choice = input("Would you like instructions? (yes/no) >")
+    if choice.lower() == 'yes':
+       print("""Bear, Ninja, Cowboy is an exciting game of strategy and skill!
+             Pit your wit against the computer! Choose a player: Bear, Ninja, or
+             Cowboy. The computer chooses a player. Bear eats Ninja, Ninja defeats
+             Cowboy and cowboy shoots bear.""".rstrip())
+    else:
+       pass
     # Get player input
-    player = input("Bear, Ninja, or Cowboy? > ")
+    selected = 0
+    score = 0
+    while selected != 1:
+        player = input("Bear, Ninja, or Cowboy? > ")
+        if (player[0].upper() + player[1:]) in roles:
+            selected += 1
+        else:
+            print("not an option")
 
     # Compare computer and player role
 
@@ -24,9 +40,11 @@ while player == False:
         print("You lose!", computer, "shoots", player)
       else: # computer is cowboy, player is ninja
         print("You win!", player, "defeats", computer)
+        score += 1
     elif computer == "Bear":
       if player == "Cowboy":
         print("You win!", player, "shoots", computer)
+        score += 1
       else: # computer is bear, player is ninja
         print("You lose!", computer, "eats", player)
     elif computer == "Ninja":
@@ -34,6 +52,7 @@ while player == False:
         print("You lose!", computer, "defeats", player)
       else: # computer is ninja, player is bear
         print("You win!", player, "eats", computer)
+        score += 1
 
     play_again = input("Would you like to play again? (yes/no) > ")
     if play_again == 'yes':
